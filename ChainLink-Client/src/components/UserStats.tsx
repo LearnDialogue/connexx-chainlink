a9a364bac1d738f1e5467311c24e771b0a60df8b
Alec Patterson <8888alec@gmail.com>
2024-10-30 12:00:40 -0400

    profile components

--- ChainLink-Client/src/components/UserStats.tsx
+++ ChainLink-Client/src/components/UserStats.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useQuery } from '@apollo/client';
+import { FETCH_USER_BY_NAME } from '../graphql/queries/userQueries';
+import { GET_HOSTED_EVENTS, GET_JOINED_EVENTS } from '../graphql/queries/eventQueries';
+import { AuthContext } from '../context/auth';
 
-interface UserStatsProps {
-  userData: {
-    getUser: {
-      FTP: number | null;
-      FTPdate: string | null;
-      weight: number | null;
-      experience: string | null;
-    };
-  } | null;
-  hostedEventsCount: number;
-  joinedEventsCount: number;
-}
+const UserStats: React.FC = () => {
+  const { user } = useContext(AuthContext);
 
-const UserStats: React.FC<UserStatsProps> = ({ userData, hostedEventsCount, joinedEventsCount }) => {
+  const { data: userData, loading: userLoading } = useQuery(FETCH_USER_BY_NAME, {
+    variables: { username: user?.username },
+    skip: !user?.username, // Skip query if username is undefined
+  });
+
+  const { data: hostedEvents, loading: hostedLoading } = useQuery(GET_HOSTED_EVENTS);
+  const { data: joinedEvents, loading: joinedLoading } = useQuery(GET_JOINED_EVENTS);
+
+  if (userLoading || hostedLoading || joinedLoading) {
+    return <div>Loading...</div>; // Display loading while fetching data
+  }
+
   return (
     <div className='profile-page-user-stats-data'>
       <div>
@@ -34,11 +39,11 @@ const UserStats: React.FC<UserStatsProps> = ({ userData, hostedEventsCount, join
       </div>
       <div>
         <div>Rides hosted</div>
-        <div>{hostedEventsCount}</div>
+        <div>{hostedEvents ? hostedEvents.getHostedEvents.length : 0}</div>
       </div>
       <div>
         <div>Rides joined</div>
-        <div>{joinedEventsCount}</div>
+        <div>{joinedEvents ? joinedEvents.getJoinedEvents.length : 0}</div>
       </div>
     </div>
   );
