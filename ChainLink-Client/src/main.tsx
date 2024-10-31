17a6da56ebedcf691f59ec6cb7d8a53238461266
Alec Patterson <8888alec@gmail.com>
2024-10-30 23:53:25 -0400

    Feature Flags (#28)
    * friend feature flagged
    
    * pull from vite env settings
    
    ---------
    
    Co-authored-by: bakermo <matthew.baker4175@gmail.com>

--- ChainLink-Client/src/main.tsx
+++ ChainLink-Client/src/main.tsx
@@ -23,6 +23,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  //enable this to debug what environment we are in
+  //console.log(import.meta.env);
   return (
     <Router>
       <AuthProvider>
