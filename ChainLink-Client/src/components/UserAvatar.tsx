65b263578177d611fef385ded2fc63453d8e3dca
bakermo <matthew.baker4175@gmail.com>
2024-11-03 17:50:09 -0500

    eliminate 404s with some future tech debt

--- ChainLink-Client/src/components/UserAvatar.tsx
+++ ChainLink-Client/src/components/UserAvatar.tsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState, useRef } from 'react';
 import AWS from 'aws-sdk';
 import Avatar from 'react-avatar';
 import featureFlags from '../featureFlags';
+import { useQuery } from '@apollo/client';
+import { FETCH_USER_BY_NAME } from '../graphql/queries/userQueries';
 
 interface UserAvatarProps {
   username: string | undefined;
-  hasProfileImage: boolean | undefined;
+  // This is tech debt for the next team. Sorry. If you know ahead of time that a user has a profile image, you can pass this prop to skip the query.
+  // Otherwise, the component will query the user to determine if they have a profile image, then cache the result of that
+  hasProfileImage?: boolean | undefined;
 }
 
 const s3 = new AWS.S3({
@@ -30,33 +34,66 @@ const UserAvatar: React.FC<UserAvatarProps> = ({
   hasProfileImage,
 }) => {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [skipQuery, setSkipQuery] = useState<boolean>(true);
   const cacheRef = useRef<{ [key: string]: { url: string, expiry: number } }>({});
 
-  if (featureFlags.profilePicturesEnabled) {
-    useEffect(() => {
-      const fetchImageUrl = async () => {
-        if (hasProfileImage && username) {
-          const cacheKey = `profile-pictures/${username}`;
-          const cachedData = cacheRef.current[cacheKey] || JSON.parse(localStorage.getItem(cacheKey) || 'null');
-  
-          if (cachedData && cachedData.expiry > Date.now()) {
-            //console.log('Using cached presigned URL:', cachedData);
-            setImageUrl(cachedData.url);
-          } else {
-            const presignedUrl = await generatePresignedUrl(cacheKey);
-            const expiry = Date.now() + 55 * 60 * 1000; // 55 minutes from now
-            const newCacheData = { url: presignedUrl, expiry };
-            cacheRef.current[cacheKey] = newCacheData;
-            localStorage.setItem(cacheKey, JSON.stringify(newCacheData));
-            //console.log('Fetched new presigned URL:', newCacheData);
-            setImageUrl(presignedUrl);
-          }
+  useEffect(() => {
+    const checkCache = () => {
+      if (username) {
+        const cacheKey = `profile-pictures/${username}`;
+        const cachedData = cacheRef.current[cacheKey] || JSON.parse(localStorage.getItem(cacheKey) || 'null');
+        if (cachedData && cachedData.expiry > Date.now()) {
+          setImageUrl(cachedData.url);
+          setSkipQuery(true); // Skip the query if cache is valid
+        } else {
+          setSkipQuery(false); // Do not skip the query if cache is invalid
         }
-      };
+      }
+    };
+    checkCache();
+  }, [username]);
+
+  const { data: userQueryData, loading, error } = useQuery(FETCH_USER_BY_NAME, { 
+    variables: { username },
+    skip: skipQuery || (hasProfileImage !== null && hasProfileImage !== undefined) // Skip if cache is valid or hasProfileImage is provided
+  });
+
+  useEffect(() => {
+    const fetchImageUrl = async () => {
+      if (username) {
+        const cacheKey = `profile-pictures/${username}`;
+        const cachedData = cacheRef.current[cacheKey] || JSON.parse(localStorage.getItem(cacheKey) || 'null');
+
+        if (cachedData && cachedData.expiry > Date.now()) {
+          setImageUrl(cachedData.url);
+        } else {
+          let profileImageStatus = hasProfileImage;
+          if (profileImageStatus === null || profileImageStatus === undefined) {
+            if (userQueryData) {
+              profileImageStatus = userQueryData.getUser.hasProfileImage;
+            } else {
+              return; // Exit early if userQueryData is not available
+            }
+          }
+
+          const expiry = Date.now() + 55 * 60 * 1000; // 55 minutes from now
+          let url = '';
+          if (profileImageStatus) {
+            url = await generatePresignedUrl(cacheKey);
+          }
+          const newCacheData = { url: url, expiry: expiry };
+          cacheRef.current[cacheKey] = newCacheData;
+          localStorage.setItem(cacheKey, JSON.stringify(newCacheData));
+          setImageUrl(url);
+        }
+      }
+    };
+
+    if (featureFlags.profilePicturesEnabled) {
       fetchImageUrl();
-    }, [hasProfileImage, username]);
-  }
-  
+    }
+  }, [hasProfileImage, username, userQueryData]);
+
   return (
     <div>
       <Avatar 
