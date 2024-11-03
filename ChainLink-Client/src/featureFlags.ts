1769a866c0b53880cd07e58290e6a1f5b6bd8d01
bakermo <matthew.baker4175@gmail.com>
2024-11-03 18:36:40 -0500

    turn on profile pics flag

--- ChainLink-Client/src/featureFlags.ts
+++ ChainLink-Client/src/featureFlags.ts
@@ -1,6 +1,6 @@
 const featureFlags = {
   friendsFeatureEnabled: import.meta.env.DEV,
-  profilePicturesEnabled: import.meta.env.DEV,
+  profilePicturesEnabled: true,
   // Add other flags here
   // some other flag when we release it:
   // nameFeatureEnabled: true,
