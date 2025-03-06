c2b6bdb281a874d40a11870e8823b0620f703878
Kevin Pham <kpham753@gmail.com>
2025-03-06 10:34:57 -0500

    Refactor imports and improve code formatting in AuthRoute, GpxHandler, PasswordValidator, and UserRoute

--- ChainLink-Client/src/util/PasswordValidator.ts
+++ ChainLink-Client/src/util/PasswordValidator.ts
@@ -1,21 +1,21 @@
 interface PasswordValidationResult {
 	isValid: boolean;
 	errorMessage?: string;
-  }
+}
 
 export const tryValidatePassword = (password: string): PasswordValidationResult => {
 	const passwordValidator =
-	  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.]).{8,}$/;
-  
+		/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.]).{8,}$/;
+
 	if (password === '') {
-	  return { isValid: false, errorMessage: 'Password is required' };
+		return { isValid: false, errorMessage: 'Password is required' };
 	} else if (!password.match(passwordValidator)) {
-	  return {
-		isValid: false,
-		errorMessage:
-		  'Passwords must be at least 8 characters, must contain at least one lowercase character, one uppercase character, one number, and one special character.',
-	  };
+		return {
+			isValid: false,
+			errorMessage:
+				'Passwords must be at least 8 characters, must contain at least one lowercase character, one uppercase character, one number, and one special character.',
+		};
 	}
-  
+
 	return { isValid: true };
-  };
+};
