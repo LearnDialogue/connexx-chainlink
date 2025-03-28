61f95c5a101d36d32b9b7f74bdceb063083ccf6f
catrinalim <catrinalim@ufl.edu>
2025-03-28 00:58:19 -0400

    edited PreviewPage to redirect logged in users to rides feed page

--- ChainLink-Client/src/routes/PreviewPage.tsx
+++ ChainLink-Client/src/routes/PreviewPage.tsx
@@ -1,19 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import PreviewEventModal from '../components/Preview';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { GET_EVENT_PREVIEW } from '../graphql/queries/previewQueries';
 import LoaderWheel from '../components/LoaderWheel';
+import { AuthContext } from '../context/auth';
+
 
 const PreviewPage = () => {
     // useParams gets token from link
     const { token } = useParams();
     
+    const navigate = useNavigate();
+    const { user } = useContext(AuthContext); 
+
     // calls query in previewQueries to fetch ride data (event and route details)
     const { loading, error, data } = useQuery(GET_EVENT_PREVIEW, {
         variables: { jwtToken: token }
     });
-    
+
+    // if user is logged in, automatically navigates to rides feed page
+    useEffect(() => {
+      if (user) {
+          navigate('/app/rides');
+      }
+    }, [user, navigate]);
+
     if (loading) return <LoaderWheel />
     if (error) {
         console.error("Get event has failed");
