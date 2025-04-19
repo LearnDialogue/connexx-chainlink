67e6c77eb25606df8d2b64e92fcb152d579a08fe
jmcgraww <127436695+jmcgraww@users.noreply.github.com>
2025-04-18 21:12:00 -0400

    Fixed non-members being able to see all existing clubs.

--- ChainLink-Client/src/components/ClubList.tsx
+++ ChainLink-Client/src/components/ClubList.tsx
@@ -1,12 +1,12 @@
-// ClubList.tsx
 import React, { useState, useEffect } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
-import { GET_CLUBS, GET_CLUB } from '../graphql/queries/clubQueries';
+import { GET_CLUBS } from '../graphql/queries/clubQueries';
 import { JOIN_CLUB, DECLINE_TO_JOIN } from '../graphql/mutations/clubMutations';
 import { FETCH_USER_BY_NAME } from '../graphql/queries/userQueries';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import '../styles/components/club-list.css';
-import featureFlags from '../featureFlags';import Button from './Button';
+import featureFlags from '../featureFlags';
+import Button from './Button';
 
 interface ClubListProps {
   username: string | null;
@@ -14,12 +14,11 @@ interface ClubListProps {
 
 const ClubList: React.FC<ClubListProps> = ({ username }) => {
   const navigate = useNavigate();
-
   const [showRequests, setShowRequests] = useState(false);
   const [clubSelected, setSelectedClub] = useState<string | null>(null);
   
   const { loading: clubsLoading, data: clubsData, refetch } = useQuery(GET_CLUBS);
-  const { loading: thisUserLoading, data: thisUserData} = useQuery(
+  const { data: thisUserData } = useQuery(
     FETCH_USER_BY_NAME,
     { variables: { username }, skip: !username }
   );
@@ -28,14 +27,19 @@ const ClubList: React.FC<ClubListProps> = ({ username }) => {
     if (refetch) refetch();
   }, [refetch]);
 
-  const [acceptClubInvitation] = useMutation(JOIN_CLUB, { onCompleted: () => setSelectedClub(null) });
+  const [acceptClubInvitation] = useMutation(JOIN_CLUB, {
+    onCompleted: () => setSelectedClub(null)
+  });
   const handleAccept = (clubId: string) => {
     acceptClubInvitation({ variables: { clubId, userId: thisUserData?.getUser?.id }});
     navigate(`/app/club/${clubId}`);
   }
 
-  const [declineClubInvitation] = useMutation(DECLINE_TO_JOIN, { onCompleted: () => setSelectedClub(null) });
-  const handleReject = (clubId: string) => declineClubInvitation({ variables: { clubId, userId: thisUserData?.getUser?.id } });
+  const [declineClubInvitation] = useMutation(DECLINE_TO_JOIN, {
+    onCompleted: () => setSelectedClub(null)
+  });
+  const handleReject = (clubId: string) =>
+    declineClubInvitation({ variables: { clubId, userId: thisUserData?.getUser?.id } });
 
   const handleListChange = (flag: boolean) => {
     setShowRequests(flag);
@@ -43,6 +47,13 @@ const ClubList: React.FC<ClubListProps> = ({ username }) => {
   };
 
   const createClub = () => navigate('/app/create/club');
+
+  const filterMemberClubs = (clubs: any[]) =>
+    clubs.filter(club =>
+      club.owners.some((o:any) => o.username === username) ||
+      club.admins.some((a:any) => a.username === username) ||
+      club.members.some((m:any) => m.username === username)
+    );
 
   return (
     <div className="profile-page-clubs-container">
@@ -97,24 +108,31 @@ const ClubList: React.FC<ClubListProps> = ({ username }) => {
               </div>
             ))
         ) : (
-          clubsData.getClubs.map((club: any) => (
-            <div
-              key={club.id}
-              className="profile-page-club-list-item"
-              onClick={() => navigate(`/app/club/${club.id}`)}
-            >
-              <span className="club-name"><b>{club.name}</b></span>
-              <span className={`club-position club-${(
-                club.owners.some((o: any) => o.username === username) ? 'owner' :
-                club.admins.some((a: any) => a.username === username) ? 'admin' :
-                'member'
-              )}`}><b>{(
-                club.owners.some((o: any) => o.username === username) ? 'Owner' :
-                club.admins.some((a: any) => a.username === username) ? 'Admin' :
-                'Member'
-              )}</b></span>
-            </div>
-          ))
+          filterMemberClubs(clubsData.getClubs)
+            .map((club: any) => (
+              <div
+                key={club.id}
+                className="profile-page-club-list-item"
+                onClick={() => navigate(`/app/club/${club.id}`)}
+              >
+                <span className="club-name"><b>{club.name}</b></span>
+                <span
+                  className={`club-position club-${
+                    club.owners.some((o:any) => o.username === username) ? 'owner' :
+                    club.admins.some((a:any) => a.username === username) ? 'admin' :
+                    'member'
+                  }`}
+                >
+                  <b>
+                    {club.owners.some((o:any) => o.username === username)
+                      ? 'Owner'
+                      : club.admins.some((a:any) => a.username === username)
+                      ? 'Admin'
+                      : 'Member'}
+                  </b>
+                </span>
+              </div>
+            ))
         )}
       </div>
     </div>
