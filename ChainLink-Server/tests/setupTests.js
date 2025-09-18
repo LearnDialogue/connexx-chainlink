7bb3772bc297cbde8b7933d80a2fffa7e41b5bf3
John Terracina <john@learndialogue.org>
2025-09-18 09:52:11 -0400

    fixed mongo env vars yet again

--- ChainLink-Server/tests/setupTests.js
+++ ChainLink-Server/tests/setupTests.js
@@ -1,11 +1,13 @@
 import { beforeEach, afterEach, vi } from 'vitest';
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const MONGODB = process.env.MONGODB || 'mongodb://127.0.0.1:27017/chainlinkDB';
 
 
 // Mock Mongoose connection
 beforeEach(async () => {
+  console.log(MONGODB);
   await mongoose.connect(MONGODB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
